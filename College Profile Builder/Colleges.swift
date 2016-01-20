8988a2e66c389fbf6dc288de93339f25e3762895
Robert McGinness <RMcGinness@roberts-macbook-air.local>
2016-01-20 12:01:52 -0600

    Created college class and added table + cell

---
+++ College Profile Builder/Colleges.swift
@@ -0,0 +1,27 @@
+//
+//  Colleges.swift
+//  College Profile Builder
+//
+//  Created by Robert McGinness on 1/20/16.
+//  Copyright © 2016 Robert McGinness. All rights reserved.
+//
+
+import UIKit
+
+class Colleges: NSObject {
+
+    var name = ""
+    var state = ""
+    var students = 0
+    var picture = UIImage(named: "nothing")
+    
+    convenience init(name: String, state: String, students: Int, picture: UIImage)
+    {
+        self.init()
+        self.name = name
+        self.state = state
+        self.students = students
+        self.picture = picture
+    }
+    
+}
