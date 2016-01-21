e857e59073c718729a71f7518e0237d2dd76d646
Robert McGinness <RMcGinness@roberts-macbook-air.local>
2016-01-21 12:22:31 -0600

    added Alert view

--- College Profile Builder/ViewController.swift
+++ College Profile Builder/ViewController.swift
@@ -12,27 +12,60 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var collegesTableView: UITableView!
     
+    @IBOutlet weak var editBarButton: UIBarButtonItem!
+    @IBOutlet weak var addBarButton: UIBarButtonItem!
+    
     var colleges : [Colleges] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        editBarButton.tag = 0
         
         colleges.append(Colleges(name: "Elmhurst College", state: "Illinois", students: 0, picture: UIImage(named: "nothing")!))
         colleges.append(Colleges(name: "John Carrol University", state: "Ohio", students: 0, picture: UIImage(named: "nothing")!))
         colleges.append(Colleges(name: "Ohio State University", state: "Ohio", students: 0, picture: UIImage(named: "nothing")!))
     }
     
-    func collegeTableView(collegesTableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(collegesTableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return colleges.count
     }
     
-    func collegeTableView(collegesTableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    func tableView(collegesTableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = collegesTableView.dequeueReusableCellWithIdentifier("myCell", forIndexPath: indexPath)
         cell.textLabel?.text = colleges[indexPath.row].name
         return cell
     }
     
+    func tableView(collegesTableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete
+        {
+            colleges.removeAtIndex(indexPath.row)
+            collegesTableView.reloadData()
+        }
+    }
 
-
+    @IBAction func onAddButtonTapped(sender: AnyObject)
+    {
+        let alert = UIAlertController(title: "Add College", message: nil, preferredStyle: .Alert)
+        alert.addTextFieldWithConfigurationHandler { (nameTextField) -> Void in
+            nameTextField.placeholder = "College Name"}
+        alert.addTextFieldWithConfigurationHandler { (locationTextField) -> Void in
+            locationTextField.placeholder = "College State"}
+        alert.addTextFieldWithConfigurationHandler { (studentsTextField) -> Void in
+            studentsTextField.placeholder = "College Students"
+            studentsTextField.keyboardType = .NumberPad}
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        alert.addAction(cancel)
+        let confirmAddition = UIAlertAction(title: "Confirm", style: .Default) { (action) -> Void in
+            
+        }
+        alert.addAction(confirmAddition)
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
 }
+
+    
+
+
 
